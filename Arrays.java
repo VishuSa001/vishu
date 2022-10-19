f006128f73eff4907f03135ce757440df8ee3397
VishuSa001 <114149406+VishuSa001@users.noreply.github.com>
2022-10-19 03:05:26 -0700

    Add files via upload

---
+++ Arrays.java
@@ -0,0 +1,52 @@
+
+/*for each loop */
+public class Arrays {
+    public static void main(String[] args) {
+        String[] name = { "vishal", "Shivam", "ankur", "Sanjay" };
+        for (int i = 0; i <= name.length; i++) {
+            System.out.println(name[i]);
+        }
+    }
+
+    // System.out.println(name[1]);
+    // System.out.println(name[2]);
+    // System.out.println(name[3]);
+    // int[] marks = { 45, 67, 78, 34, 56 };
+    // System.out.println("Using For Each Loop execution");
+    // for (int i = marks.length - 1; i >= 0; i--) {
+    // System.out.println(marks[i]);
+    // }
+    // // for (int i : marks) {
+    // System.out.println(marks.length);
+    // }
+}
+
+// Display the marks
+// System.out.println("Marks of student:");
+// for (int i = 0; i <= marks.length; i++) {
+// System.out.println(marks[i]);
+// }
+
+// Display reverse order of Student marks
+// for (int i = marks.length - 1; i >= 0; i--) {
+// System.out.println(marks[i]);
+// }
+
+// float[] marks2 = { 45.6f, 67, 69.5f, 89, 90 };
+// System.out.println(marks2[4]);
+// System.out.println(marks.length);
+// System.out.println("Length of students and indexing");
+// String[] Student = { "vishal", " shivam", "shiva","Ajay" };
+
+// System.out.println(Student.length);
+// System.out.println(Student[2]);
+// // int marks[] = new int[5];
+// marks[0] = 38;
+// marks[1] = 30;
+// marks[2] = 56;
+// marks[3] = 88;
+// marks[4] = 98;
+// // System.out.println(marks[0]);
+// for (int i = 0; i <= 4; i++) {
+// System.out.println(marks[i]);
+// }
